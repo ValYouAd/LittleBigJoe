761786727493e5547d7068e23be23595c4aa9d84
laurent <laurent.brieu@hotmail.fr>
2013-09-19 17:41:30 +0200

    "Launch my project" section + bug fixes + wording + blog part for project

---
+++ src/LittleBigJoe/Bundle/FrontendBundle/Form/EntryType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace LittleBigJoe\Bundle\FrontendBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class EntryType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', 'text', array(
+        				'label' => 'Entry title'
+        		))
+            ->add('content', 'ckeditor', array(
+            		'label' => 'Entry content',
+            		'data' => $options['data']->getContent(),
+            		'toolbar' => array('clipboard', 'basicstyles', 'links', 'insert', 'tools'),
+            		'toolbar_groups' => array(
+            				'document' => array(),
+            				'clipboard' => array('Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo'),
+            				'editing' => array(),
+            				'basicstyles' => array('Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat'),
+            				'paragraph' => array('NumberedList','BulletedList','-','Outdent','Indent','-','JustifyLeft', 'JustifyCenter','JustifyRight','JustifyBlock'),
+		            		'links' => array('Link','oembed', 'Unlink','Anchor'),
+		            		'insert' => array('Table'),
+		            		'styles' => array(),
+		            		'tools' => array('Maximize')
+            		)
+        		))
+            ->add('isPublic', 'choice', array(
+        				'label' => 'Visible to non participants ?',
+            		'choices' => array('0' => 'No', '1' => 'Yes')
+        		))
+        		->add('addEntry', 'submit', array(
+            		'label' => 'Add entry'
+            ))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'LittleBigJoe\Bundle\CoreBundle\Entity\Entry'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'entry';
+    }
+}
