3aa1bba4eec6870f9e6ca5cd162cef6d8b395dff
Pierrick Martos <pierrick@martos.me>
2014-06-02 18:30:15 +0200

    restrict access

---
+++ web/access.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: pierrickmartos
+ * Date: 31/05/2013
+ * Time: 12:08
+ * To change this template use File | Settings | File Templates.
+ */
+
+if (isset($_POST['password']) && !empty($_POST['password']) && ($_POST['password'] == 'lbj@2014')) {
+    $fp = fopen(__DIR__.'/../ip.ini', 'a');
+    fwrite($fp, 'ip[] = '.$_SERVER['REMOTE_ADDR']."\n");
+    echo $_SERVER['REMOTE_ADDR'];
+    fclose($fp);
+
+    header('Location: http://dev.littlebigjoe.com/');
+}
+
+?>
+<form method="POST">
+    <input type="password" name="password">
+    <input type="submit">
+</form>
