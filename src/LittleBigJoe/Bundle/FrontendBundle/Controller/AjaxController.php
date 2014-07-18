99fccbb0fec35069d6b7fdb0a4474741292fc65c
LaurentBrieu <laurentbrieu@gmail.com>
2014-07-18 14:12:27 +0200

    #126 : Unlike project

--- src/LittleBigJoe/Bundle/FrontendBundle/Controller/AjaxController.php
+++ src/LittleBigJoe/Bundle/FrontendBundle/Controller/AjaxController.php
@@ -994,6 +994,71 @@ class AjaxController extends Controller
     		return new JsonResponse(array('status' => 'OK'));
     		exit;
     }
+
+    /**
+     * Unlike a project
+     *
+     * @Route("/unlike-project", name="littlebigjoe_frontendbundle_ajax_unlike_project")
+     * @Method("POST")
+     * @Template()
+     */
+    public function unlikeProjectAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $projectId = (int)$this->get('request')->request->get('projectId');
+
+        // If it's not a correct project id
+        if (empty($projectId))
+        {
+            return new JsonResponse(array('status' => 'KO ID'));
+        }
+
+        $project = $em->getRepository('LittleBigJoeCoreBundle:Project')->find($projectId);
+
+        // If the project doesn't exist
+        if (empty($project))
+        {
+            return new JsonResponse(array('status' => 'KO PROJECT'));
+        }
+
+        $currentUser = $this->get('security.context')->getToken()->getUser();
+        // If the current user is not logged, redirect him to login page
+        if (!is_object($currentUser))
+        {
+            $this->get('session')->getFlashBag()->add(
+                'notice',
+                'You must be logged in to unlike this project'
+            );
+
+            // Force base url to make sure environment is not specified in the URL
+            $this->get('router')->getContext()->setBaseUrl('');
+            $request->getSession()->set('_security.main.target_path', $this->generateUrl('littlebigjoe_frontendbundle_project_show', array('id' => $project->getId(), 'slug' => $project->getSlug())).'?likePopup=true');
+            return new JsonResponse(array('status' => 'KO USER'));
+        }
+
+        $projectLikeExists = $em->getRepository('LittleBigJoeCoreBundle:ProjectLike')->findOneBy(array(
+            'project' => $project->getId(),
+            'user' => $currentUser->getId()
+        ));
+
+        // If user has already liked the project
+        if (empty($projectLikeExists) || !($projectLikeExists instanceof ProjectLike))
+        {
+            return new JsonResponse(array('status' => 'KO VOTE'));
+        }
+
+        // Remove like in DB
+        $projectLikes = $project->getLikesCount();
+        $project->setLikesCount($projectLikes - 1);
+        $em->persist($project);
+
+        $em->remove($projectLikeExists);
+        $em->flush();
+
+        // Make sure no code is executed after it
+        return new JsonResponse(array('status' => 'OK'));
+        exit;
+    }
     
     /**
      * Fund a project
