4fb255832e117473811ef5ab45b63e4f62ad23d3
huas <huas@valyouad.com>
2014-08-04 18:06:52 +0200

    #177 ExceptionController override

--- src/LittleBigJoe/Bundle/FrontendBundle/Controller/Error403Controller.php
+++ src/LittleBigJoe/Bundle/FrontendBundle/Controller/Error403Controller.php
@@ -4,16 +4,45 @@ namespace LittleBigJoe\Bundle\FrontendBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use LittleBigJoe\Bundle\CoreBundle\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use FOS\UserBundle\FOSUserEvents;
+
 
 class Error403Controller extends Controller
 {
-    public function codeBetaAction(Request $request) {
+    public function editCodeBetaAction(Request $request) {
+  /*      $user = $this->container->get('security.context')->getToken()->getUser();
+        if (!is_object($user) || !$user instanceof User) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        $dispatcher = $this->container->get('event_dispatcher');
+
+        $event = new GetResponseUserEvent($user, $request);
+        $dispatcher->dispatch(FOSUserEvents::PROFILE_EDIT_INITIALIZE, $event);
+
+        if (null !== $event->getResponse()) {
+            return $event->getResponse();
+        }*/
+
         $user = $request->getUser();
 
         $form = $this->createFormBuilder($user)
                 ->add('betaCodeValue', 'text', array('label' => ('Get your beta access now!'), 'required' => false))
                 ->add('submitCode', 'submit', array('label' => ('Submit')))
                 ->getForm();
+
+        $form->handleRequest($request);
+
+        echo $request->getMethod();
+        if ($form->isSubmitted()) echo "test";
+        if ('POST' === $request->getMethod()) {
+            if ($form->isValid()) {
+//                echo "test";die;
+//              return $this->redirect($this->generateUrl('task_success'));
+            }
+        }
 
         return $this->render('LittleBigJoeFrontendBundle:Error403:error403.html.twig', array(
             'form' => $form->createView(),
---
+++ src/LittleBigJoe/Bundle/FrontendBundle/Controller/TwigExceptionController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace LittleBigJoe\Bundle\FrontendBundle\Controller;
+
+use Symfony\Bundle\TwigBundle\Controller\ExceptionController as BaseController;
+use Symfony\Bundle\FrameworkBundle\Templating\TemplateReference;
+use Symfony\Component\HttpKernel\Exception\FlattenException;
+use Symfony\Component\HttpKernel\Log\DebugLoggerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TwigExceptionController extends BaseController {
+
+    public function showAction(Request $request, FlattenException $exception, DebugLoggerInterface $logger = null, $_format = 'html')
+    {
+        $currentContent = $this->getAndCleanOutputBuffering($request->headers->get('X-Php-Ob-Level', -1));
+
+        $code = $exception->getStatusCode();
+
+        return new Response($this->twig->render(
+            $this->findTemplate($request, $_format, $code, $this->debug),
+            array(
+                'status_code'    => $code,
+                'status_text'    => isset(Response::$statusTexts[$code]) ? Response::$statusTexts[$code] : '',
+                'exception'      => $exception,
+                'logger'         => $logger,
+                'currentContent' => $currentContent,
+            )
+        ));
+    }
+}
