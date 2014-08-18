4d20e8974fbf80b678c10a0c9136e37b07cbc70b
LaurentBrieu <laurentbrieu@gmail.com>
2014-08-18 11:17:40 +0200

    #188 : Delete comments

--- src/LittleBigJoe/Bundle/FrontendBundle/Controller/AjaxController.php
+++ src/LittleBigJoe/Bundle/FrontendBundle/Controller/AjaxController.php
@@ -1342,6 +1342,47 @@ class AjaxController extends Controller
         return new JsonResponse(array('status' => 'OK'));
         exit;
     }
+
+    /**
+     * Delete a entry comment
+     *
+     * @Route("/delete-entry-comment", name="littlebigjoe_frontendbundle_ajax_delete_entry_comment")
+     * @Method("POST")
+     * @Template()
+     */
+    public function deleteEntryCommentAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $commentId = $this->get('request')->request->get('commentId');
+
+        // If there's no comment id
+        if (empty($commentId))
+        {
+            return new JsonResponse(array('status' => 'KO FIELD'));
+        }
+
+        $currentUser = $this->get('security.context')->getToken()->getUser();
+        // If the current user is not logged, redirect him to login page
+        if (!is_object($currentUser) || (!$this->get('security.context')->isGranted('ROLE_ADMIN')))
+        {
+            return new JsonResponse(array('status' => 'KO USER'));
+        }
+
+        $comment = $em->getRepository('LittleBigJoeCoreBundle:EntryComment')->find($commentId);
+
+        if (empty($comment) || !($comment instanceof EntryComment))
+        {
+            return new JsonResponse(array('status' => 'KO FIELD'));
+        }
+
+        // Delete comment in DB
+        $em->remove($comment);
+        $em->flush();
+
+        // Make sure no code is executed after it
+        return new JsonResponse(array('status' => 'OK'));
+        exit;
+    }
     
     /**
      * Upload new project file
